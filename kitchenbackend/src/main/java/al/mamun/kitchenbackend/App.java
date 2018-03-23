1fbd370b23b7b2107bd3181f0ec8d97975774e49
almamun <archermamun@gmail.com>
2018-03-23 16:56:46 +0600

    My Project Setup completed

---
+++ kitchenbackend/src/main/java/al/mamun/kitchenbackend/App.java
@@ -0,0 +1,13 @@
+package al.mamun.kitchenbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
